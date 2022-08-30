857ea25681aa4326058eeef355b28d5bc1e298df
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-08-30 00:34:23 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -85,5 +88,44 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return null;
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("select seller.*, department.Name as DepName from seller inner join department on seller.DepartmentId = department.id where DepartmentId = ? order by Name");
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                // Irá ser guardado dentro do método todo o departamento que for instanciado.
+                // Será pesquisado todos os departamentos que estão presentes.
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                // Caso não seja null, quer dizer que sim tem pessoas.
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+
+                Seller obj = instantiateSeller(rs, dep);
+
+                list.add(obj);
+            }
+
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 }
